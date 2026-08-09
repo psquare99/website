7e65b49279afc12260d33b4d13982cbbf5d318aa
Prateek <30472009+psquare99@users.noreply.github.com>
2026-08-09 19:29:38 +0530

    journal: delete "p"

--- content/journal/reflections/index.ts
+++ content/journal/reflections/index.ts
@@ -1,11 +1,8 @@
 import { themanifesto } from "./themanifesto";
-import { p } from "./p";
-  import { whyJournalExists } from "./why-journal-exists";
+import { whyJournalExists } from "./why-journal-exists";
 
   export const reflections = [
     whyJournalExists,
   
   themanifesto,
-
-  p,
 ];
