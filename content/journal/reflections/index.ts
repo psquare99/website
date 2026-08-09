373b600d988351c3d1040f0c99382239ef2942ff
Prateek <30472009+psquare99@users.noreply.github.com>
2026-08-09 13:54:42 +0530

    journal: publish "Hello"

--- content/journal/reflections/index.ts
+++ content/journal/reflections/index.ts
@@ -1,8 +1,11 @@
 import { themanifesto } from "./themanifesto";
+import { p } from "./p";
   import { whyJournalExists } from "./why-journal-exists";
 
   export const reflections = [
     whyJournalExists,
   
   themanifesto,
+
+  p,
 ];
