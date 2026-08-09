5ca84f898addb1482599a15961a91460c03745c4
Prateek <30472009+psquare99@users.noreply.github.com>
2026-08-09 09:19:03 +0530

    journal: publish "The Manifesto"

--- content/journal/reflections/index.ts
+++ content/journal/reflections/index.ts
@@ -1,5 +1,8 @@
+import { themanifesto } from "./themanifesto";
   import { whyJournalExists } from "./why-journal-exists";
 
   export const reflections = [
     whyJournalExists,
-  ];
+  
+  themanifesto,
+];
