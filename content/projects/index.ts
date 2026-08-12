41ba3971b8ccb2a9576c9ee1768842ea4b8eb43f
Prateek <prateekpal99@gmail.com>
2026-08-12 23:38:25 +0545

    chore: retire journal publisher from website

--- content/projects/index.ts
+++ content/projects/index.ts
@@ -2,14 +2,14 @@ import { adiKailash } from "./adi-kailash";
 import { prime } from "./prime";
 import { statementAnalyzer } from "./statement-analyzer";
 import { wayfarer } from "./wayfarer";
-import { journalPublisher } from "./journal-publisher";
+
 import { curio } from "./curio";
 
 export const projects = [
   curio,
   adiKailash,
   prime,
-  journalPublisher,
+ 
   statementAnalyzer,
   wayfarer,
 ];
